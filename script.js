2eab93b5967a347a1418dca8a2a5bb7c6951a695
MirafuentesD <denilsonmirafuentes3@gmail.com>
2023-07-25 16:16:27 -0500

    code styling update

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ let context = canvas.getContext("2d");
 const windowHeight = window.innerHeight / 3;
 const windowWidth = window.innerWidth;
 
-canvas.height=windowHeight;
-canvas.width=windowWidth;
+canvas.height = windowHeight;
+canvas.width = windowWidth;
 
 const stopBarX = windowWidth * .75;
 const stopBarWidth = 15;
 var noteTracker = 2;
 
-const allKeys = document.querySelectorAll(['.whiteKey','.blackKey']);
+const allKeys = document.querySelectorAll(['.whiteKey', '.blackKey']);
 
-const scale1 = ["C", "D", "E", "F", "G", "A", "B","C2"];
+const scale1 = ["C", "D", "E", "F", "G", "A", "B", "C2"];
 
 var notes = []
 
@@ -31,7 +31,7 @@ var gameLost = false;
 
 
 class MusicNote {
-    constructor(note,xpos,ypos,radius,color,speed){
+    constructor(note, xpos, ypos, radius, color, speed) {
         this.xpos = xpos;
         this.ypos = ypos;
         this.note = note;
@@ -43,137 +43,136 @@ class MusicNote {
         this.dy = 1 * this.speed;
 
     }
-    draw(context){
+    draw(context) {
         context.beginPath();
         context.strokeStyle = "black";
         context.textAlign = "center";
         context.textBaseline = "middle";
         context.font = "40px Arial";
-        context.fillStyle="black";
-        context.fillText(this.note,this.xpos,this.ypos);
-        context.arc(this.xpos,this.ypos,this.radius,0,Math.PI * 2,false);
+        context.fillStyle = "black";
+        context.fillText(this.note, this.xpos, this.ypos);
+        context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
         context.stroke();
         context.closePath();
     }
 
-    drawError(context){
+    drawError(context) {
         context.beginPath();
         context.strokeStyle = "red";
         context.textAlign = "center";
         context.textBaseline = "middle";
         context.font = "40px Arial";
-        context.fillText(this.note,this.xpos,this.ypos);
-        context.arc(this.xpos,this.ypos,this.radius,0,Math.PI * 2,false);
+        context.fillText(this.note, this.xpos, this.ypos);
+        context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
         context.stroke();
         context.closePath();
     }
 
-    update(){
-        if (parseInt(this.xpos) + this.radius <= Math.floor(stopBarX)){
+    update() {
+        if (parseInt(this.xpos) + this.radius <= Math.floor(stopBarX)) {
             this.draw(context);
         }
-        else
-        {
+        else {
             this.drawError(context);
             gameLost = 'true';
         }
         this.xpos += this.dx;
 
     }
-} 
+}
 
 
 const updateNotes = function () {
-    if (notes.length == 0 &&  frame.style.display==''){
-        frame.style.display='none';
-        canvas.style.display='none';
-        welcomeBox.style.display='none';
-        resultsSection.style.display='';
-        restartButton.style.display='';
-        if(gameLost){
-            resultsSection.innerText=":( unsuccessful " + fNnameInput.value + " missed a note";
-            resultsSection.style.border ='2px solid red';
-            
+    if (notes.length == 0 && frame.style.display == '') {
+        frame.style.display = 'none';
+        canvas.style.display = 'none';
+        welcomeBox.style.display = 'none';
+        resultsSection.style.display = '';
+        restartButton.style.display = '';
+        if (gameLost) {
+            resultsSection.innerText = ":( unsuccessful " + fNnameInput.value + " missed a note";
+            resultsSection.style.border = '2px solid red';
+
         }
-        else{
-            resultsSection.innerText= "Congrats, " + fNnameInput.value +'! You completed the scale';
+        else {
+            resultsSection.innerText = "Congrats, " + fNnameInput.value + '! You completed the scale';
         }
     }
-    else if ( notes.length > 1 && (parseInt(notes[notes.length-1].xpos) > windowWidth)) {
+    else if (notes.length > 1 && (parseInt(notes[notes.length - 1].xpos) > windowWidth)) {
         notes = [];
         gameLost = true;
-        frame.style.display=='';
+        frame.style.display == '';
     }
-    else{
-        for (let i = 0; i < notes.length; i++){
+    else {
+        for (let i = 0; i < notes.length; i++) {
             notes[i].update();
-            }
-        
+        }
+
     }
 }
 
 const keyClicked = (Event) => {
     var audio = new Audio('./soundFiles/piano_' + Event.currentTarget.id + '_major.wav');
     audio.play();
-    if (Event.currentTarget.id==(notes[0].note)){
+    if (Event.currentTarget.id == (notes[0].note)) {
         notes.shift();
     }
 }
 
-for (let i = 0; i < allKeys.length; i++){
-    allKeys[i].addEventListener('click',keyClicked);
+for (let i = 0; i < allKeys.length; i++) {
+    allKeys[i].addEventListener('click', keyClicked);
 };
 
 
 let drawRectangle = function () {
     context.beginPath();
-    context.fillStyle="red";
-    context.fillRect(stopBarX, 0, stopBarWidth, windowHeight );
+    context.fillStyle = "red";
+    context.fillRect(stopBarX, 0, stopBarWidth, windowHeight);
     context.stroke();
     context.closePath();
-    
+
 }
 
-let getNameStartGame = function(){
-    frame.style.display='none';
-    canvas.style.display='none';
-    welcomeBox.style.display='none';
-    resultsSection.style.display='none';
-    restartButton.style.display='none';
+let getNameStartGame = function () {
+    frame.style.display = 'none';
+    canvas.style.display = 'none';
+    welcomeBox.style.display = 'none';
+    resultsSection.style.display = 'none';
+    restartButton.style.display = 'none';
 }
 
 
-let updateCircle = function() {
+let updateCircle = function () {
     requestAnimationFrame(updateCircle);
-    context.clearRect(0,0,windowWidth,windowHeight);
+    context.clearRect(0, 0, windowWidth, windowHeight);
     updateNotes();
     drawRectangle();
 }
-  
+
 
 document.querySelector('#fNameInput').addEventListener('keypress', function (e) {
     if (e.key === 'Enter' && fNnameInput.value != '') {
-        frame.style.display='';
-        canvas.style.display='';
-      // code for enter
-        welcomeBox.style.display='';
-        namePrompt.style.display='none';
+        frame.style.display = '';
+        canvas.style.display = '';
+        // code for enter
+        welcomeBox.style.display = '';
+        namePrompt.style.display = 'none';
         welcomeBox.innerText = welcomeBox.innerText + ' ' + fNnameInput.value + '!';
-        for (let i = 0; i < scale1.length; i++){
-            var note = new MusicNote(scale1[i], (-150 * i) + windowWidth * .45 ,100,50,"black",2);
+        for (let i = 0; i < scale1.length; i++) {
+            var note = new MusicNote(scale1[i], (-150 * i) + windowWidth * .45, 100, 50, "black", 2);
             note.draw(context);
             notes.push(note);
-        } 
+        }
     }
 });
 
 document.querySelector('#restartButton').addEventListener('click', function (e) {
-       getNameStartGame();
-       namePrompt.style.display='';
-       fNnameInput.value='';
-       welcomeBox.innerText = 'Welcome';
-       gameLost = false;
-       updateCircle();
+    getNameStartGame();
+    namePrompt.style.display = '';
+    fNnameInput.value = '';
+    welcomeBox.innerText = 'Welcome';
+    gameLost = false;
+    updateCircle();
 });
 
 
